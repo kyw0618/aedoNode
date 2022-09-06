d71cbef562582d40e6332645014e964f685029e8
KKKYW <hopan678@gmail.com>
2022-09-06 18:05:25 +0900

    s

--- node/router/obituary.js
+++ node/router/obituary.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/', isAuth, upload, obitController.createObituary);
 router.put('/:id', isAuth, obitController.updateObit);
 router.delete('/:id', isAuth, obitController.removeObit);
+
 router.get('/', isAuth, obitController.getByname);
 router.get('/my', isAuth, obitController.getMyObituary);
 router.get('/image', isAuth, obitController.getImageData);
