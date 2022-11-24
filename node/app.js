501b807220b137c7290ab2b5717ccf3365f5f66c
KKKYW <hopan678@gmail.com>
2022-11-24 13:13:18 +0900

    ㅈ

--- node/app.js
+++ node/app.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import morgan from 'morgan';
+import fs from 'fs';
+import Https from 'https';
 import helmet from 'helmet';
 import cors from 'cors'
-import { config } from './config.js';
 import { connectDB } from './db/db.js';
 import appRouter from './router/app.js';
 import userRouter from './router/auth.js';
@@ -19,6 +20,11 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('tiny'));
 
+const options = { // letsencrypt로 받은 인증서 경로를 입력
+  ca: fs.readFileSync('/etc/letsencrypt/live/www.aedo.co.kr/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/www.aedo.co.kr/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/www.aedo.co.kr/cert.pem')
+  };
 app.use('/v1/app', appRouter);
 app.use('/v1/user', userRouter);
 app.use('/v1/obituary', obituaryRouter);
@@ -37,5 +43,5 @@ app.use((error, req, res, next) => {
 
 connectDB().then(() => {
   console.log(`Server is started... ${new Date()}`);
-  app.listen(config.port);
+  Https.createServer(options, app).listen(443);
 })
